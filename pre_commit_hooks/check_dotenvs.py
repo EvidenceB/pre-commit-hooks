3f0ff59fffa84cb1f7c09f1381b451ffb4843ed5
bruno <martin.bruno.mail@gmail.com>
2020-04-21 22:46:42 +0200

    add debug

--- pre_commit_hooks/check_dotenvs.py
+++ pre_commit_hooks/check_dotenvs.py
@@ -36,7 +36,9 @@ def main():
     statuses = []
     for sample in files_ending_with_sample:
         prefix = sample.replace(".sample", "")
+        print(sample, prefix)
         prefixed_files = [f for f in files if f in look_for(prefix)]
+        print(prefixed_files)
         data = {}
         whole_set = set()
         for f in prefixed_files:
